f59c77355302a0de14e6a69b28b9e87d815c5bbb
plr5034 <46530118+plr5034@users.noreply.github.com>
2025-01-23 10:09:22 -0600

    added repr

--- homework1.py
+++ homework1.py
@@ -1,7 +1,7 @@
 '''
 Homework1
 Name: Paul Ring
-github link: 
+github link: https://github.com/plr5034/Python_ADD-160/blob/main/homework1.py
 Note: Remember to use comments for each function.
 doc strings should include what each input consists of, 
 what the expected output is and a description of the function.
@@ -10,13 +10,19 @@ what the expected output is and a description of the function.
 class Bank_Account:
     def __init__(self,name,starting_amount):
         # initializing attributes for Bank_Account
+        # The constructor has two input variables, the name of the account and the starting amount. 
+        # Assign each of these values to instance variables.
 
+        self.accountName = name
+        self.starting_amount = starting_amount
 
         pass
     
     def __repr__(self):
         # printable representation of the object.   Formatting is not its virtue.  This is more for troubleshooting
-        return 
+        # for the homework, sample output should look like:
+        #         Bank_Account(name='Bob',  Account Balance=12457.22)
+        return f"Bank_Account*(name={self.accountName},(Account Balance={self.starting_amount}))"
     
     def __str__(self):
         # Pretty Print version of the object.   Good for user, more easily readable.
