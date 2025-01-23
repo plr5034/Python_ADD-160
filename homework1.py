81c9c6dc6f09ec3e24b6361319eb3241006b764e
plr5034 <46530118+plr5034@users.noreply.github.com>
2025-01-23 11:19:56 -0600

    first pass

--- homework1.py
+++ homework1.py
@@ -14,7 +14,7 @@ class Bank_Account:
         # Assign each of these values to instance variables.
 
         self.accountName = name
-        self.starting_amount = starting_amount
+        self.balance = starting_amount
 
         pass
     
@@ -22,21 +22,41 @@ class Bank_Account:
         # printable representation of the object.   Formatting is not its virtue.  This is more for troubleshooting
         # for the homework, sample output should look like:
         #         Bank_Account(name='Bob',  Account Balance=12457.22)
-        return f"Bank_Account*(name={self.accountName},(Account Balance={self.starting_amount}))"
+        return f"Bank_Account*(name={self.accountName}),(Account Balance={self.balance})"
     
     def __str__(self):
         # Pretty Print version of the object.   Good for user, more easily readable.
-        return 
+        return f"Account Name: {self.accountName}\nAccount Balance: {self.balance}"
     
     def deposit(self,amount):
-        # your code here
+        # Deposit function 
+        # If the amount deposited is greater than zero, add it to the account. 
+        # Otherwise, don't allow the amount to change. 
+        if amount > 0:
+            self.balance += amount
+            print (f"{amount} deposited. New Balance: {self.balance}")
+        else:
+            print ("Please deposit a positive number.")
         pass
     
     def withdraw(self,amount):
+        # Withdraw function 
+        # If the amount withdrawn is greater than zero and it doesn't overdraw the account, 
+        # allow the amount to be withdrawn. Otherwise, don't allow the withdraw.
+        if amount > 0 and amount <= self.balance:
+            self.balance -= amount
+            print(f"{amount} withdrawn. New Balance: {self.balance}")
+        elif amount > self.balance:
+            print("Insufficient funds.")
+        else:
+            print("Please withdraw an amount greater than zero.")
         pass
 
     
     def check_balance(self):
+        # Check_balance function 
+        # This function will print out the balance of the account holder.      
+        print(f"Balance: {self.balance}")  
         pass
 
 if __name__ == "__main__":
