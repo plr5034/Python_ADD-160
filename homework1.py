a24539e50f79b64b52ae0a17dd8e9bd6c0e1b5cd
plr5034 <46530118+plr5034@users.noreply.github.com>
2025-01-23 11:53:39 -0600

    Final successful version.  Note: The b in New Balance was upper case in written example, but test had lower case so changed it to lower case.

--- homework1.py
+++ homework1.py
@@ -12,52 +12,50 @@ class Bank_Account:
         # initializing attributes for Bank_Account
         # The constructor has two input variables, the name of the account and the starting amount. 
         # Assign each of these values to instance variables.
-
         self.accountName = name
-        self.balance = starting_amount
-
+        self.account = int(starting_amount)
         pass
     
     def __repr__(self):
         # printable representation of the object.   Formatting is not its virtue.  This is more for troubleshooting
         # for the homework, sample output should look like:
         #         Bank_Account(name='Bob',  Account Balance=12457.22)
-        return f"Bank_Account*(name={self.accountName}),(Account Balance={self.balance})"
+        return f"Bank_Account(name='{self.accountName}', Account Balance={self.account})"
     
     def __str__(self):
         # Pretty Print version of the object.   Good for user, more easily readable.
-        return f"Account Name: {self.accountName}\nAccount Balance: {self.balance}"
+        return f"Account Name: {self.accountName}\nAccount Balance: {self.account}"
     
     def deposit(self,amount):
         # Deposit function 
         # If the amount deposited is greater than zero, add it to the account. 
         # Otherwise, don't allow the amount to change. 
         if amount > 0:
-            self.balance += amount
-            print (f"{amount} deposited. New Balance: {self.balance}")
+            self.account += amount
+            print (f"{amount} deposited. New balance: {self.account}")
         else:
             print ("Please deposit a positive number.")
         pass
-    
+
     def withdraw(self,amount):
         # Withdraw function 
         # If the amount withdrawn is greater than zero and it doesn't overdraw the account, 
         # allow the amount to be withdrawn. Otherwise, don't allow the withdraw.
-        if amount > 0 and amount <= self.balance:
-            self.balance -= amount
-            print(f"{amount} withdrawn. New Balance: {self.balance}")
-        elif amount > self.balance:
+        if amount > 0 and amount <= self.account:
+            self.account -= amount
+            print(f"{amount} withdrawn. New balance: {self.account}")
+        elif amount > self.account:
             print("Insufficient funds.")
         else:
             print("Please withdraw an amount greater than zero.")
         pass
-
     
     def check_balance(self):
         # Check_balance function 
         # This function will print out the balance of the account holder.      
-        print(f"Balance: {self.balance}")  
+        print(f"Balance: {self.account}")  
         pass
+
 
 if __name__ == "__main__":
     import doctest
