ab6f98bfcd0bcf325e36ebc63acafe981b982bdd
plr5034 <46530118+plr5034@users.noreply.github.com>
2025-01-23 11:57:02 -0600

    Updated a few of the comments

--- homework1.py
+++ homework1.py
@@ -12,6 +12,8 @@ class Bank_Account:
         # initializing attributes for Bank_Account
         # The constructor has two input variables, the name of the account and the starting amount. 
         # Assign each of these values to instance variables.
+        # NOTE: remember to change type to int for the starting amount.  This will prevent errors later on.
+
         self.accountName = name
         self.account = int(starting_amount)
         pass
@@ -20,16 +22,19 @@ class Bank_Account:
         # printable representation of the object.   Formatting is not its virtue.  This is more for troubleshooting
         # for the homework, sample output should look like:
         #         Bank_Account(name='Bob',  Account Balance=12457.22)
+
         return f"Bank_Account(name='{self.accountName}', Account Balance={self.account})"
     
     def __str__(self):
         # Pretty Print version of the object.   Good for user, more easily readable.
+
         return f"Account Name: {self.accountName}\nAccount Balance: {self.account}"
     
     def deposit(self,amount):
         # Deposit function 
         # If the amount deposited is greater than zero, add it to the account. 
         # Otherwise, don't allow the amount to change. 
+
         if amount > 0:
             self.account += amount
             print (f"{amount} deposited. New balance: {self.account}")
@@ -41,6 +46,7 @@ class Bank_Account:
         # Withdraw function 
         # If the amount withdrawn is greater than zero and it doesn't overdraw the account, 
         # allow the amount to be withdrawn. Otherwise, don't allow the withdraw.
+
         if amount > 0 and amount <= self.account:
             self.account -= amount
             print(f"{amount} withdrawn. New balance: {self.account}")
@@ -53,9 +59,11 @@ class Bank_Account:
     def check_balance(self):
         # Check_balance function 
         # This function will print out the balance of the account holder.      
+
         print(f"Balance: {self.account}")  
         pass
 
+# The following code shouldn't be changed as it runs the automated checking
 
 if __name__ == "__main__":
     import doctest
